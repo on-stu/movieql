bbc9f8ba90416cd0d5b6797b407959be65d8b4e5
on-stu <minsu0523@naver.com>
2021-12-09 08:54:36 +0900

    setting before starting mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,14 @@
-import { getById, people } from "../db";
+import { getById, getMovies } from "../db";
 
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, { id }) => {
+    movies: () => getMovies(),
+    movie: (_, { id }) => {
       //첫번째 인자는 무슨 object라는데 쓸모는 없다고 함
       return getById(id);
     },
   },
+  Mutation: {},
 };
 
 export default resolvers;
