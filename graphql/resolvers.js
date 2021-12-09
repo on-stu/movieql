bad07ab0cf274a46f78d65c4cf2219d2af88b0ae
on-stu <minsu0523@naver.com>
2021-12-09 09:23:17 +0900

    wrapping a REST API with GraphQL

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,8 @@
-import { addMovie, getById, getMovies } from "../db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
     movies: () => getMovies(),
-    movie: (_, { id }) => {
-      //첫번째 인자는 무슨 object라는데 쓸모는 없다고 함
-      return getById(id);
-    },
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
   },
 };
 
